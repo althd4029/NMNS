1c0531a35e8aa5f188e93c7c46d709e2cbaf9737
althd4029 <83136595+althd4029@users.noreply.github.com>
2021-09-27 14:33:21 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+let categorycolor = document.getElementById("categorycolor");
+let categorydropcolor = document.getElementById("categorydropcolor");
+
+/*
+let go_earing = document.getElementById("go_earing");
+
+go_earing.addEventListener("click", goToEaringPage);
+
+function goToEaringPage(){
+
+}
+
+*/
+
